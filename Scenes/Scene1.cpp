33ee371820b055ec371c8dcf8eafbde74ac545c5
Jesse Li <jesseli@Macmini.fritz.box>
2025-10-31 16:39:51 +0100

    First commit and small changes

--- Scenes/Scene1.cpp
+++ Scenes/Scene1.cpp
@@ -1,1 +1,23 @@
 #include "Scene1.h"
+#include <glm/gtx/quaternion.hpp>
+
+void Scene1::onDraw(Renderer& renderer){
+    renderer.drawWireCube(glm::vec3(0), glm::vec3(5), glm::vec3(1));
+
+    renderer.drawCube(  glm::vec3(0),
+                        glm::quat(glm::vec3(pitch, roll, yaw)), 
+                        glm::vec3(0.5, 0.5, 0.5), 
+                        glm::vec4(1, 0, 0, 1));
+
+    glm::mat4 rotation = glm::toMat4(glm::quat(glm::vec3(pitch, roll, yaw)));
+    glm::vec3 forward = glm::vec3(rotation * glm::vec4(0, 0, 1, 0));
+    glm::vec3 right = glm::vec3(rotation * glm::vec4(1, 0, 0, 0));
+    glm::vec3 up = glm::vec3(rotation * glm::vec4(0, 1, 0, 0));
+}
+
+void Scene1::simulateStep(){
+    pitch += 0.001f;
+    roll += 0.002f;
+    yaw += 0.003f;
+}
+
--- Scenes/Scene1.h
+++ Scenes/Scene1.h
@@ -2,4 +2,10 @@
 
 class Scene1 : public Scene
 {
+    float pitch = 0.f;
+    float roll = 0.f;
+    float yaw = 0.f;
+
+    virtual void onDraw(Renderer &renderer) override;
+    virtual void simulateStep() override;
 };
