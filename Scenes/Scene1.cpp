a18ff2db79cadf69a75ea6ccaf56db0bde15f8be
Jesse Li <lijesse@umich.edu>
2025-10-31 22:59:32 +0100

    submission

--- Scenes/Scene1.cpp
+++ Scenes/Scene1.cpp
@@ -1,23 +1,109 @@
 #include "Scene1.h"
 #include <glm/gtx/quaternion.hpp>
+#include <imgui.h>
 
 void Scene1::onDraw(Renderer& renderer){
     renderer.drawWireCube(glm::vec3(0), glm::vec3(5), glm::vec3(1));
-
-    renderer.drawCube(  glm::vec3(0),
+    auto cmap = Colormap("viridis");
+    
+    renderer.drawCube(  glm::vec3(0,0,0), 
                         glm::quat(glm::vec3(pitch, roll, yaw)), 
-                        glm::vec3(0.5, 0.5, 0.5), 
-                        glm::vec4(1, 0, 0, 1));
+                        glm::vec3(0.5,0.5,0.5), 
+                        glm::vec4(1,0,0,1));
 
     glm::mat4 rotation = glm::toMat4(glm::quat(glm::vec3(pitch, roll, yaw)));
     glm::vec3 forward = glm::vec3(rotation * glm::vec4(0, 0, 1, 0));
     glm::vec3 right = glm::vec3(rotation * glm::vec4(1, 0, 0, 0));
     glm::vec3 up = glm::vec3(rotation * glm::vec4(0, 1, 0, 0));
+
+    cameraMatrix = renderer.camera.viewMatrix;
+    fwd = inverse(cameraMatrix) * glm::vec4(0, 0, 1, 0);
+    right = inverse(cameraMatrix) * glm::vec4(1, 0, 0, 0);
+    up = inverse(cameraMatrix) * glm::vec4(0, 1, 0, 0);
+
+    renderer.drawLine(glm::vec3(0), forward, glm::vec4(1, 0, 0, 1));
+    renderer.drawLine(glm::vec3(0), right, glm::vec4(0, 1, 0, 1));
+    renderer.drawLine(glm::vec3(0), up, glm::vec4(0, 0, 1, 1));
+
+    for (auto& particle : particles){
+        renderer.drawSphere(particle.position, 0.1f, glm::vec4(cmap(particle.lifetime / 1.5f), 1));
+    }
+}
+
+void Scene1::onGUI(){
+    ImGui::SliderFloat("Pitch Increment", &pitch_increment, -0.01f, 0.01f);
+    ImGui::SliderFloat("Roll Increment", &roll_increment, -0.01f, 0.01f);
+    ImGui::SliderFloat("Yaw Increment", &yaw_increment, -0.01f, 0.01f);
+    ImGui::SliderInt("Launch Delay", &launch_delay, 0, 100);
+    auto launch = ImGui::Button("Launch");
+
+    if(launch){
+        launchSphere();
+    }
+}
+
+void Scene1::launchSphere(){
+    if( lastLaunch < launch_delay)
+        return;
+    lastLaunch = 0;
+    glm::mat4 rotation = glm::toMat4(glm::quat(glm::vec3(pitch, roll, yaw)));
+    glm::vec3 forward = glm::vec3(rotation * glm::vec4(0, 0, 1, 0));
+    glm::vec3 right = glm::vec3(rotation * glm::vec4(1, 0, 0, 0));
+    glm::vec3 up = glm::vec3(rotation * glm::vec4(0, 1, 0, 0));
+
+    glm::vec4 color = glm::vec4(dis(gen), dis(gen), dis(gen), 1);
+    float velocityMagnitude = 4.5f + dis(gen);
+    glm::vec3 velocity = forward * velocityMagnitude;
+
+    velocity += right * (dis(gen) - 0.5f) * 2.f;
+    velocity += up * (dis(gen) - 0.5f) * 2.f;
+
+    particles.push_back(Particle{glm::vec3(0), velocity, color, .0});
 }
 
 void Scene1::simulateStep(){
-    pitch += 0.001f;
-    roll += 0.002f;
-    yaw += 0.003f;
+    //pitch += pitch_increment;
+    //roll += roll_increment;
+    //yaw += yaw_increment;
+    float realtimeDt = ImGui::GetIO().DeltaTime;
+    glm::vec3 gravityAccel = glm::vec3(0, 0, -9.81f);
+
+    for (auto& particle : particles){
+        particle.position += realtimeDt * particle.velocity;
+        particle.lifetime += realtimeDt;
+        particle.velocity += gravityAccel * realtimeDt;
+    }
+    
+    particles.erase(std::remove_if(particles.begin(), particles.end(), [](const Particle& particle){
+        return particle.lifetime > 4.5f;
+    }), particles.end());
+
+
+    if(ImGui::IsKeyDown(ImGuiKey_Space))
+        launchSphere();
+    if(ImGui::IsKeyDown(ImGuiKey_W))
+        pitch += pitch_increment;
+    if(ImGui::IsKeyDown(ImGuiKey_S))
+        pitch -= pitch_increment;
+    if(ImGui::IsKeyDown(ImGuiKey_A))
+        roll += roll_increment;
+    if(ImGui::IsKeyDown(ImGuiKey_D))
+        roll -= roll_increment;
+    if(ImGui::IsKeyDown(ImGuiKey_Q))
+        yaw += yaw_increment;
+    if(ImGui::IsKeyDown(ImGuiKey_E))
+        yaw -= yaw_increment;
+    lastLaunch++;
+
+    if(ImGui::IsMouseReleased(ImGuiMouseButton_Right)){   
+        auto drag = ImGui::GetMouseDragDelta(1);
+        if(!(drag.x == 0 && drag.y == 0)){
+            auto dx = drag.x * right;
+            auto dy = -drag.y * up;
+            for (auto& particle : particles){
+                particle.velocity += (dx + dy) * 0.01f;
+            }
+        }
+    }
 }
 
--- Scenes/Scene1.h
+++ Scenes/Scene1.h
@@ -1,11 +1,49 @@
 #include "Scene.h"
+#include <random>
+
+
+struct Particle
+{
+    glm::vec3 position;
+    glm::vec3 velocity;
+    glm::vec4 color;
+    float lifetime;
+};
 
 class Scene1 : public Scene
 {
+    std::vector<Particle> particles;
+
+    std::random_device rd;
+    std::mt19937 gen;
+    std::uniform_real_distribution<float> dis;
+
     float pitch = 0.f;
     float roll = 0.f;
     float yaw = 0.f;
 
+    float pitch_increment = 0.001f;
+    float roll_increment = 0.002f;
+    float yaw_increment = 0.003f;
+
+    int32_t launch_delay = 8;
+    int32_t lastLaunch = 0;
+
+    glm::mat4 cameraMatrix = glm::mat4(1);
+    glm::vec3 fwd = glm::vec3(1, 0, 0);
+    glm::vec3 right = glm::vec3(0, 1, 0);
+    glm::vec3 up = glm::vec3(0, 0, 1);
+
+
+    void launchSphere();
+
+    virtual void onGUI() override;
+
     virtual void onDraw(Renderer &renderer) override;
     virtual void simulateStep() override;
+
+    public:
+        Scene1() : gen(rd()), dis(0.f, 1.f) {}
+
 };
+
